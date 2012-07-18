bfaa76eb2d17bc7ef949232acfef199dcefa9a1a
Woojin Han <verecahill.@gmail.com>
2012-07-18 15:43:17 +0900

    finish demo app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 DemoApp::Application.routes.draw do
+  resources :microposts
+
+  resources :users
+
+  root :to => 'welcome#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
